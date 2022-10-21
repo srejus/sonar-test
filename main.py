d6f14a46f867a9c63024108c3121cc2b90223798
Srejus Jayakumar <82509096+srejus@users.noreply.github.com>
2022-10-21 10:08:35 +0530

    second commit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello world!")
+USER = "admin"
+PASSWORD = "admin"
+
+usr = input("Enter your Username: ")
+passw = input("Enter your Password: ")
+
+if usr == USER and PASSWORD == passw:
+    print("Logged in")
+else:
+    print("Incorrect creds!")
